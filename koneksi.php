da266f7e4cc2dd964965c465c4798393ea3ef101
ranggagilang <gilangyanuarirangga@gmail.com>
2025-03-30 01:10:55 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,12 +3,12 @@
 $host = getenv('MYSQLHOST') ?: 'localhost';
 $user = getenv('MYSQLUSER') ?: 'root';
 $pass = getenv('MYSQLPASSWORD') ?: '';
-$db = getenv('MYSQLDATABASE') ?: 'db_toko_boneka';
+$db = getenv('MYSQLDATABASE') ?: 'railway'; // Ubah dari 'db_toko_boneka' ke 'railway'
 $port = getenv('MYSQLPORT') ?: '3306';
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+    die("koneksi gagal: " . mysqli_connect_error());
 }
 ?>
